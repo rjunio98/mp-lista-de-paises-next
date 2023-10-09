8d3e97144886f4867b8c7ed7120dd4565df43362
rjunio98 <robledorjunior1@gmail.com>
2023-10-09 15:33:55 -0300

    criando card do país

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+import Image from "next/image";
+
 type Country = {
   name: {
     common: string;
+  };
+
+  translations: {
+    por: {
+      common: string;
+    };
+  };
+
+  flags: {
+    svg: string;
+    alt: string;
   };
 };
 
@@ -16,10 +29,21 @@ export default async function Home() {
     <section className="grid grid-cols-5 w-full container gap-2 mt-16">
       {countries.map((country) => (
         <article
-          className="h-64 min-w-full p-2 bg-white border-2 rounded-xl"
+          className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 transition-all hover:shadow-xl"
           key={country.name.common}
         >
-          <h1>{country.name.common}</h1>
+          <div className="relative w-full h-40 p2 overflow-hidden rounded-xl">
+            <Image
+              src={country.flags.svg}
+              alt={country.flags.alt}
+              fill
+              className="object-cover"
+            />
+          </div>
+
+          <h1 className="font-bold text-xl text-center mt-1">
+            {country.translations.por.common}
+          </h1>
         </article>
       ))}
     </section>
