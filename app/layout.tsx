6b9c00866b0cec3b20b7428b2785bd1a2cf34829
rjunio98 <robledorjunior1@gmail.com>
2023-10-09 15:12:38 -0300

    fazendo fetch dados no Server Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,38 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Nunito_Sans } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Nunito_Sans } from "next/font/google";
+import Image from "next/image";
 
-const nunitoSans = Nunito_Sans({ subsets: ['latin'] })
+const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Lista de países',
-  description: 'Uma lista de países criada com o Next 13',
-}
+  title: "Lista de países",
+  description: "Uma lista de países criada com o Next 13",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-br">
-      <body className={nunitoSans.className}>{children}</body>
+      <body className={nunitoSans.className}>
+        <main className="bg-gray-100 min-h-screen flex flex-col items-center">
+          <nav className="w-full bg-white h-16 flex items-start justify-center">
+            <section className="container flex items-center gap-3">
+              <Image
+                width={48}
+                height={48}
+                src="/logo.svg"
+                alt="Logo da aplicação - emoji de globo"
+              />
+              <h1 className="font-bold text-2xl">Lista de países</h1>
+            </section>
+          </nav>
+          {children}
+        </main>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
-import Image from 'next/image'
+type Country = {
+  name: {
+    common: string;
+  };
+};
 
-export default function Home() {
+async function getCountries(): Promise<Country[]> {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  return response.json();
+}
+
+export default async function Home() {
+  const countries = await getCountries();
+
   return (
-    <h1>Home</h1>
-  )
+    <section className="grid grid-cols-5 w-full container gap-2 mt-16">
+      {countries.map((country) => (
+        <article
+          className="h-64 min-w-full p-2 bg-white border-2 rounded-xl"
+          key={country.name.common}
+        >
+          <h1>{country.name.common}</h1>
+        </article>
+      ))}
+    </section>
+  );
 }
